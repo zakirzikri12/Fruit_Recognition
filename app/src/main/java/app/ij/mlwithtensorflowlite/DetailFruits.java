384e7145dd4ad3ee3b420152cdd584715983df55
MOHAMAD ZAKIR ZIKRI BIN IBRAHIM <80703252+zakirzikri12@users.noreply.github.com>
2023-07-04 21:33:24 +0800

    15th commit: update ui detail fruits

--- app/src/main/java/app/ij/mlwithtensorflowlite/DetailFruits.java
+++ app/src/main/java/app/ij/mlwithtensorflowlite/DetailFruits.java
@@ -37,6 +37,9 @@ public class DetailFruits extends AppCompatActivity {
         TextView txtT = findViewById(R.id.txtTitle);
         ImageView imgT = findViewById(R.id.imgTitle);
         TextView txtS = findViewById(R.id.txtStory);
+        TextView txtS1 = findViewById(R.id.txtStory1);
+        TextView txtS2 = findViewById(R.id.txtStory2);
+
 
         Bundle extras = getIntent().getExtras();
         String story  = "Default Story Text";
@@ -46,27 +49,37 @@ public class DetailFruits extends AppCompatActivity {
             if(story.equals("storyA")){
                 txtT.setText("Durian: D101");
                 imgT.setImageResource(R.drawable.d101);
-                txtS.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit nisi nec quam condimentum, sit amet consequat lorem fermentum. Integer dictum");
+                txtS.setText("Known as Durian IOI");
+                txtS1.setText("Characteristics: dull yellow and slightly sweet and creamy. The texture of this type of durian is very soft.Fruits from old trees carry a mild astringent aftertaste");
+                txtS2.setText("Others: the price is also not too expensive when compared to other premium types of durian");
             }
             else if (story.equals("storyB")){
                 txtT.setText("Durian: D197");
                 imgT.setImageResource(R.drawable.d197);
-                txtS.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit nisi nec quam condimentum, sit amet consequat lorem fermentum. Integer dictum");
+                txtS.setText("Known as Durian Musang king (king of fruit)");
+                txtS1.setText("Characteristics: The king is known for its thick flesh, small core and melts-in-your-mouth goodness. its long finish has layered complexity, bittersweet in a savoury aftertaste");
+                txtS2.setText("Other: Musang King is often considered one of the best varieties in terms of taste and texture");
             }
             else if (story.equals("storyC")){
                 txtT.setText("Durian: D24");
                 imgT.setImageResource(R.drawable.d24);
-                txtS.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit nisi nec quam condimentum, sit amet consequat lorem fermentum. Integer dictum");
+                txtS.setText("Also known as Durian Sultan or Durian Bukit Merah");
+                txtS1.setText("Characteristics: Its creamy texture has wholesome yeasty sweetness with a pleasant astringent pull on tongue (strong bitter-sweet). A connoisseur's favorite");
+                txtS2.setText("Others: one of the ways to identify durian D24 is that there is an imperfect space in the middle of the D24 square ");
             }
             else if (story.equals("storyD")){
                 txtT.setText("Durian: D2");
                 imgT.setImageResource(R.drawable.d2);
-                txtS.setText("Durian D2 known as Durian sultan");
+                txtS.setText("Durian D2 known as Dato Nina");
+                txtS1.setText("Characteristics: The fruit is medium in size, slightly oval and the skin is bronze green. The spines of the fruit are short, the skin is thin but difficult to open if the new fruit falls. Fruit stalks are short and large");
+                txtS2.setText("Others: Its golden finish features a mild astringent note complimented by a layer of delicate sweetness");
             }
             else if (story.equals("storyE")){
                 txtT.setText("Durian: D175");
                 imgT.setImageResource(R.drawable.d175);
-                txtS.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit nisi nec quam condimentum, sit amet consequat lorem fermentum. Integer dictum");
+                txtS.setText("Known as Red Prawn");
+                txtS1.setText("Characteristics: Usually brown skinned, this durian is oval-shaped, short-thorned, and quite difficult to peel because of its thick and hard skin. Tips: The brownish-green skin of Red Shrimp is said to be grown outside of Penang");
+                txtS2.setText("Others:  Dense and concenteres, the Red Prawn has bloom of rich, pungent flavors that leads into a mild astringent aftertaste.");
             }
         }
         btnBack.setOnClickListener(new View.OnClickListener() {
